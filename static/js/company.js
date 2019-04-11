8f729328379c1369da84c479451fe27e4312c047
Roman Collins <romancollins30@gmail.com>
2019-04-10 21:46:19 -0700

    4/10

--- static/js/company.js
+++ static/js/company.js
@@ -1,41 +1,36 @@
 // Store API query variables
-var url = "/api/company_detail";
+var name = 'MMM'
+var url = "/api/company_detail/" + name;
 
-d3.json(url, function (error,data) {
-console.log("inside table");
-  function tabulate(data, columns) {
-		var table = d3.select('#company_table').append('table')
-		var thead = table.append('thead')
-		var	tbody = table.append('tbody');
-
-		// append the header row
-		thead.append('tr')
-		  .selectAll('th')
-		  .data(columns).enter()
-		  .append('th')
-		    .text(function (column) { return column; });
+d3.json(url, function (data) {
+console.log(data);
+  //function tabulate(data) {
+		//var table = d3.select('#company_table').append('table')
+		//var	tbody = table.append('tbody');
 
 		// create a row for each object in the data
-		var rows = tbody.selectAll('tr')
-		  .data(data)
-		  .enter()
-		  .append('tr');
-
+		var rows = [data];
+	d3.select("tbody")
+		.selectAll("tr")
+	  .data(rows)
+	  .enter()
+	  .append("tr")
+		.html(function(d) {
+			return `<td>${d.city}</td>`
+		});
+		
 		// create a cell in each row for each column
-		var cells = rows.selectAll('td')
-		  .data(function (row) {
-		    return columns.map(function (column) {
-		      return {column: column, value: row[column]};
-		    });
-		  })
-		  .enter()
-		  .append('td')
-		    .text(function (d) { return d.value; });
+		// rows.selectAll('tr')
+		//   .data(data)
+		//   .enter()
+		//   .append('td')
+		// 	.text(data['comp_desc']);
+		// console.log(data['comp_desc']);
 
-	  return table;
-	}
+	  //return table;
+	//}
 
 	// render the table(s)
-	tabulate(data, ['date', 'close']); // 2 column table
+	//tabulate(data); // 2 column table
 
 });
