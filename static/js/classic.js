80224f4fa747e083b80a342f0dd1ce110f407568
Roman Collins <romancollins30@gmail.com>
2019-04-19 07:39:35 -0700

    4/19

--- static/js/classic.js
+++ static/js/classic.js
@@ -12,6 +12,7 @@ function handleChange(event) {
 
 e.on("change", handleChange);
 
+//creates stock info table
 function buildMetadata(ticker) {
 
   // Build the metadata panel
@@ -29,6 +30,7 @@ function buildMetadata(ticker) {
     });
 }
 
+//creates stock charts
 function buildCharts(ticker) {
 
     const url = "/api/master12/" + ticker;
@@ -119,7 +121,7 @@ d3.json(url2).then(function(data) {
         },
         yaxis: {
           title: {
-            text: 'Number of Shared Traded',
+            text: 'Number of Shares Traded',
             font: {
               family: 'Arial',
               size: 18,
@@ -190,17 +192,52 @@ d3.json(url3).then(function(data) {
 
 
 
-var doc = new jsPDF()
+//function to create PDF with table
 var creat_pdf = d3.select("#pdf")
-
 creat_pdf.on('click', function() {
-  var source = window.document.getElementsByTagName("div")[0];
-  doc.text(20, 20, 'Stock information');
+  var pick = document.getElementById("stock_menu");
+  var stock = pick.options[pick.selectedIndex].value;
+  const url = "/api/master12/" + stock;
 
-  doc.fromHTML(
-    source,
-    15,
-    15
-    );
-  doc.save("your_stock");
+  d3.json(url).then(function(data) {
+    var doc = new jsPDF()
+    // var source = window.document.getElementsByTagName("div");
+    doc.text(50, 20, 'Stock Data For The Past 12 Months');
+    doc.setFontSize(9);
+    doc.setLineHeightFactor(1.75);
+    var ticker = data.map(d => d.Ticker);
+    var date = data.map(d => d.Date);
+    var high = data.map(d => d.High);
+    var low = data.map(d => d.Low);
+    var open = data.map(d => d.Open);
+    var close = data.map(d => d.Close);
+    var volume = data.map(d => d.Volume);
+    
+    //changing date to strings for the autotable function
+    for(i=0; i<date.length; i++) {
+      high[i] = String(high[i]);
+      low[i] = String(low[i]);
+      open[i] = String(open[i]);
+      close[i] = String(close[i]);
+      volume[i] = String(volume[i]);
+    }
+    var body = [];
+    
+    //pushing data in to the body array
+    for(i=0; i<date.length; i++) {
+      body.push([ticker[i],date[i],high[i],low[i],open[i],close[i],volume[i]])
+    }
+    
+    //autotable function 
+    doc.autoTable({
+      startY: 30,
+      head: [
+          ['Ticker', 'Date', 'High', 'Low', 'Open', 'Close', 'Volume']
+      ],
+      body: body
+
   });
+    //downloads PDF to client
+    doc.save("your_stock");
+  });
+});
