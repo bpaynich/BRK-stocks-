8d3046d5dc270c3e18cc8ed05c9a70ad00f42ffd
ktaliah <ktaliah57@gmail.com>
2019-05-08 22:46:12 -0700

    added dynamic scatterplots

--- static/js/modern2.js
+++ static/js/modern2.js
@@ -1,59 +1,27 @@
-// function buildTable() {
+const url = "api/companies_details";
+var x = '';
+var y = '';
+var xtext = '';
+var ytext = '';
 
-//   // Build the stock data table
-//   const stockUrl = "api/stock_changes";
- 
-//   let tbody = d3.select("#stock_data");
-//   tbody.html("");
-    
-//     d3.json(stockUrl).then(function(stockData) {
-//       console.log(stockData)
-//       tbody.append("thead")
-//       Object.keys(stockData[0]).forEach(function(key) {
-//         //tbody.append("th");
-//         tbody.append("th").text(key);
-//       });
-//       Object.entries(stockData).forEach(function([key, value]) {
-//         console.log(key);
-//         console.log(value);
-//         tbody.append("tr");
-//         //tbody.append("td").text(`${key}`);
-//         tbody.append("td").text(value["index"]);
-//         tbody.append("td").text(value["Stock Name"]);
-//         tbody.append("td").text(value["Highest Percent Change"] + "%");
-//         tbody.append("td").text(value["Date of Highest Percent Change"]);
-//         tbody.append("td").text(value["Lowest Percent Change"] + "%");
-//         tbody.append("td").text(value["Date of Lowest Percent Change"]);
-//         tbody.append("td").text(value["Highest Average Change for a Specific Date"] + "%");
-//         tbody.append("td").text(value["Date of Highest Average Change"]);
-//         tbody.append("td").text(value["Lowest Average Change for a Specific Date"] + "%");
-//         tbody.append("td").text(value["Date of Lowest Average Change"]);
-//       });
-//     });
-//  };
- 
-//  buildTable();
-
-const url = "/api/companies_details";
-
-d3.json(url).then(function(data) {
-    
-      // Build a Chart
+d3.json(url).then(function (data) {
+  // Function to build charts
+  function buildCharts() {
+    if (x && y ) {
     var trace1 = {
-      x: data.map(d=>d.market_val),
-      y: data.map(d=>d.emp_cnt),
+      x: data.map(d => d[x]),
+      y: data.map(d => d[y]),
       mode: 'markers',
       type: 'bubble',
-      text: data.map(d=>d.comp_name_2),
+      text: data.map(d => d.comp_name_2),
       textposition: 'top center',
       textfont: {
-        family:  'Raleway, sans-serif'
+        family: 'Raleway, sans-serif'
       },
       marker: { size: 12 }
     };
-    
-    var data = [ trace1 ];
-    
+    var data1 = [trace1];
+
     var layout = {
       legend: {
         y: 0.5,
@@ -64,10 +32,10 @@ d3.json(url).then(function(data) {
           color: 'grey',
         }
       },
-      title:'Market Value (Millions) Vs. Employee Count',
+      title: `${xtext} vs ${ytext}`,
       xaxis: {
         title: {
-          text: 'Market Value (Millions)',
+          text: `${xtext}`,
           font: {
             family: 'Arial',
             size: 18,
@@ -77,7 +45,7 @@ d3.json(url).then(function(data) {
       },
       yaxis: {
         title: {
-          text: 'Employee Count',
+          text: `${ytext}`,
           font: {
             family: 'Arial',
             size: 18,
@@ -85,233 +53,30 @@ d3.json(url).then(function(data) {
           }
         }
       }
-      
+
     };
-    
-    Plotly.newPlot('scatter_plot1', data, layout);
 
-});
+    Plotly.newPlot('adaptive_plot', data1, layout);
+    };
 
+  };
 
+  // Function to handle input change
+  function handleChange() {
+    x = xPick.options[xPick.selectedIndex].value;
+    y = yPick.options[yPick.selectedIndex].value;
+    xtext = xPick.options[xPick.selectedIndex].text;
+    ytext = yPick.options[yPick.selectedIndex].text;
+    console.log(x, y);
+    // d3.select("adaptive_plot").attr("class","panel-body table table-sm");
+    buildCharts();
+  };
 
+  var xPick = document.getElementById("stock_parameters_x");
+  var yPick = document.getElementById("stock_parameters_y");
 
-// const url = "/api/companies_details";
+  xPick.onchange = handleChange;
+  yPick.onchange = handleChange;
 
-d3.json(url).then(function(data) {
-
- // Build a Chart
-var trace2 = {
- x: data.map(d=>d.price_per_sales),
- y: data.map(d=>d.price_cash),
- mode: 'markers',
- type: 'scatter',
- text: data.map(d=>d.comp_name_2),
- textposition: 'top center',
- textfont: {
-   family:  'Raleway, sans-serif'
- },
- marker: { size: 12 }
-};
-
-var data = [ trace2 ];
-
-var layout2 = {
- legend: {
-   y: 0.5,
-   yref: 'paper',
-   font: {
-     family: 'Arial, sans-serif',
-     size: 20,
-     color: 'grey',
-   }
- },
- title:'Price-Sales Ratio vs. Price-to-Cash Flow Ratio',
- xaxis: {
-   title: {
-     text: 'Price-Sales Ratio',
-     font: {
-       family: 'Arial',
-       size: 18,
-       color: '#000000'
-     }
-   },
- },
- yaxis: {
-   title: {
-     text: 'Price-to-Cash Flow Ratio',
-     font: {
-       family: 'Arial',
-       size: 18,
-       color: '#000000'
-     }
-   }
- }
- 
-};
-
-Plotly.newPlot('scatter_plot2', data, layout2);
-
-});
-
-
-
-
-// const url = "/api/companies_details";
-
-d3.json(url).then(function(data) {
-
- // Build a Chart
-var trace3 = {
- x: data.map(d=>d.market_val),
- y: data.map(d=>d.tot_revenue_f0),
- mode: 'markers',
- type: 'bubble',
- text: data.map(d=>d.comp_name_2),
- textposition: 'top center',
- textfont: {
-   family:  'Raleway, sans-serif'
- },
- marker: { size: 12 }
-};
-
-var data = [ trace3 ];
-
-var layout3 = {
- legend: {
-   y: 0.5,
-   yref: 'paper',
-   font: {
-     family: 'Arial, sans-serif',
-     size: 20,
-     color: 'grey',
-   }
- },
- title:'Market Value (Millions) Vs. Total Revenue (Millions)',
- xaxis: {
-   title: {
-     text: 'Market Value (Millions)',
-     font: {
-       family: 'Arial',
-       size: 18,
-       color: '#000000'
-     }
-   },
- },
- yaxis: {
-   title: {
-     text: 'Total Revenue (Millions)',
-     font: {
-       family: 'Arial',
-       size: 18,
-       color: '#000000'
-     }
-   }
- }
- 
-};
-
-Plotly.newPlot('scatter_plot3', data, layout3);
-
-});
-
-// const url = "/api/companies_details";
-
-d3.json(url).then(function(data) {
-var trace4 = {
-  x: data.map(d=>d.zacks_m_ind_desc),
-  y: data.map(d=>d.tot_revenue_f0),
-  type: 'bar',
-  text: data.map(d=>d.comp_name_2),
-  textposition: 'top center',
-  textfont: {
-    family:  'Raleway, sans-serif'
-  },
-  marker: { size: 12 }
- };
- var data = [ trace4 ];
-
- var layout4 = {
-  legend: {
-    y: 0.5,
-    yref: 'paper',
-    font: {
-      family: 'Arial, sans-serif',
-      size: 20,
-      color: 'grey',
-    }
-  },
-  title:'Industry Type Vs. Total Revenue (Millions)',
-  xaxis: {
-    title: {
-      text: 'Industry Type',
-      font: {
-        family: 'Arial',
-        size: 18,
-        color: '#000000'
-      }
-    },
-  },
-  yaxis: {
-    title: {
-      text: 'Total Revenue (Millions)',
-      font: {
-        family: 'Arial',
-        size: 18,
-        color: '#000000'
-      }
-    }
-  }
-  
- };
-Plotly.newPlot('bar_graph1', data, layout4);
-});
-
-d3.json(url).then(function(data) {
-  var trace5 = {
-    x: data.map(d=>d.state_code),
-    y: data.map(d=>d.market_val),
-    type: 'bar',
-    text: data.map(d=>d.comp_name_2),
-    textposition: 'top center',
-    textfont: {
-      family:  'Raleway, sans-serif'
-    },
-    marker: { size: 12 }
-   };
-var data = [ trace5 ];
-
-var layout5 = {
- legend: {
-   y: 0.5,
-   yref: 'paper',
-   font: {
-     family: 'Arial, sans-serif',
-     size: 20,
-     color: 'grey',
-   }
- },
- title:'State Vs. Market Value (Millions)',
- xaxis: {
-   title: {
-     text: 'State',
-     font: {
-       family: 'Arial',
-       size: 18,
-       color: '#000000'
-     }
-   },
- },
- yaxis: {
-   title: {
-     text: 'Market Value (Millions)',
-     font: {
-       family: 'Arial',
-       size: 18,
-       color: '#000000'
-     }
-   }
- }
- 
-};
-Plotly.newPlot('bar_graph2', data, layout5);
+  buildCharts();
 });
